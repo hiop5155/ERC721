19994d21a4d30ee53645bc0e670cd24189fe91e7
unknown <hiop5155@gmail.com>
2022-01-25 09:00:33 +0800

    add transformfrom function

--- src/contracts/ERC721.sol
+++ src/contracts/ERC721.sol
@@ -18,8 +18,10 @@ contract ERC721{
     //token id => owner
     mapping(uint256 => address) private _tokenOwner;
     //owner => number of ownde tokens
-    mapping(address => uint256) private _OwndeTokensCount;
-    
+    mapping(address => uint256) private _OwnedTokensCount;
+    //mapping from token id to approved address
+    mapping(uint256 => address) private _tokenApprovals;
+
     /// @notice Count all NFTs assigned to an owner
     /// @dev NFTs assigned to the zero address are considered invalid, and this
     ///  function throws for queries about the zero address.
@@ -27,7 +29,7 @@ contract ERC721{
     /// @return The number of NFTs owned by `_owner`, possibly zero
     function balanceOf(address _owner) public view returns(uint){
         require(_owner != address(0), 'owner can not be 0');
-        return _OwndeTokensCount[_owner];
+        return _OwnedTokensCount[_owner];
     }
     /// @notice Find the owner of an NFT
     /// @dev NFTs assigned to zero address are considered invalid, and queries
@@ -55,8 +57,31 @@ contract ERC721{
         require(to != address(0),"mint address can't be 0!");
         require(!_exists(tokenId), "REC721: token already minted");
         _tokenOwner[tokenId] = to;
-        _OwndeTokensCount[to] += 1;
+        _OwnedTokensCount[to] += 1;
 
         emit Transfer(address(0), to, tokenId);
     }
+
+    /// @notice Transfer ownership of an NFT -- THE CALLER IS RESPONSIBLE
+    ///  TO CONFIRM THAT `_to` IS CAPABLE OF RECEIVING NFTS OR ELSE
+    ///  THEY MAY BE PERMANENTLY LOST
+    /// @dev Throws unless `msg.sender` is the current owner, an authorized
+    ///  operator, or the approved address for this NFT. Throws if `_from` is
+    ///  not the current owner. Throws if `_to` is the zero address. Throws if
+    ///  `_tokenId` is not a valid NFT.
+    /// @param _from The current owner of the NFT
+    /// @param _to The new owner
+    /// @param _tokenId The NFT to transfer
+    function _transferFrom(address _from, address _to, uint256 _tokenId) internal{
+        require(ownerOf(_tokenId) == _from,'You are not the owner');
+        require(_to != address(0),'can not send to NULL address');
+        _tokenOwner[_tokenId] = _to;
+        _OwnedTokensCount[_from] -= 1;
+        _OwnedTokensCount[_to] += 1;
+
+        emit Transfer(_from, _to, _tokenId);
+    }
+    function transferFrom(address _from, address _to, uint256 _tokenId) public{
+        _transferFrom(_from, _to, _tokenId);
+    } 
 }
